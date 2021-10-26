f01f1677cb8c3764340e7bace455b732b2b6706d
grant <gmacmil3@my.centennialcollege.ca>
2021-10-25 21:16:43 -0400

    updated URI for mongo atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://grant:Macmilg2660@midtermdbserver.imksv.mongodb.net/books229?retryWrites=true&w=majority"
 };
